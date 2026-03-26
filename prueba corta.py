89b320055055d8eb66530cef691303b6eb765e0e
Josephespanita12 <josephgamer778ep@gmail.com>
2026-03-26 13:32:46 -0600

    Add files via upload

---
+++ prueba corta.py
@@ -0,0 +1,37 @@
+numero1=float(input("ngrese un numero"))#se pide un numero y se usa float para ver si se ingresan decimales
+if numero1>0:
+    print("numero es positivo")
+
+
+edad=int(input("ingrese su edad"))#se piede la edad para pasarlo entero y else es para decir si el numero no es el correcto aparezca menor de edad
+if edad>=18:
+    print("mayor de edad")
+else:
+    print("menor de edad")
+
+
+
+
+nota=int(input("ingrese su nota"))
+if nota>=90 and nota<=100:
+    print("excelente")
+elif nota>=70 and nota<=89:
+    print("bueno")
+elif nota>=60 and nota<=69:
+    print("regular")
+elif nota<=60:
+    print("reprobado")
+
+
+numero = int(input("ingrese numero : "))
+if numero >= 0:
+     print("numero es positivo")
+     if numero< 0:
+         print("numero es negativo")
+numero=int(input("ingrese numero : "))
+if numero > 0:
+         print("numero es positivo")
+if numero< 0:
+            print("numero es negativo")
+
+
