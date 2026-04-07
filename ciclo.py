878df26dff38e3745f41c895e1e72bde83b6acf4
Josephespanita12 <josephgamer778ep@gmail.com>
2026-04-07 12:29:38 -0600

    Add files via upload

---
+++ ciclo.py
@@ -0,0 +1,15 @@
+for i in range(1,7):
+    print(i)
+
+
+for y in range(1,200):
+      print(y)
+
+for x in range(1,150):
+    print(x)
+
+for z in range(1,60):
+    print(z)
+
+for v in range(1,100):
+    print(v)
---
+++ ciclofor.psc
@@ -0,0 +1,9 @@
+Algoritmo ciclofor
+	Definir recorrido como entero
+	para recorrido <- 0 Hasta 200 Con Paso 1 Hacer
+		Escribir recorrido
+	FinPara
+	
+FinAlgoritmo
+	
+	
